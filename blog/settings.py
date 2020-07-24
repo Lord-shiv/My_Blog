8cce3cb1c57a16e82cb4b1c543804c98a91b8cc6
Shiv_Boy <shivamchouhan728@gmail.com>
2020-07-24 10:54:07 +0530

    Updated STATIC_ROOT

--- blog/settings.py
+++ blog/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Tinymce
